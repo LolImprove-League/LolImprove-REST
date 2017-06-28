c82becdc87c35540ef04e49204071ad415bd1ccd
LennartVanDamme <lennart.vandamme@gmail.com>
2017-06-28 08:09:20 +0200

    Finished API for Champion Mastery information. Ready to Start on League controller

---
+++ src/main/java/com/lolimprove/model/dto/ChamptionMasteryListDTO.java
@@ -0,0 +1,22 @@
+package com.lolimprove.model.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
+
+/**
+ * Created by LVDBB73 on 28/06/2017.
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ChamptionMasteryListDTO {
+
+    private List<ChampionMasteryDTO> championMasteryDTOS;
+
+    public List<ChampionMasteryDTO> getChampionMasteryDTOS() {
+        return championMasteryDTOS;
+    }
+
+    public void setChampionMasteryDTOS(List<ChampionMasteryDTO> championMasteryDTOS) {
+        this.championMasteryDTOS = championMasteryDTOS;
+    }
+}
